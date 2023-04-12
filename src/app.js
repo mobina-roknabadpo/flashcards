fb0ac2328c684d6b5ebf8c620cf95ce4bcf3851d
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-04-12 19:33:28 +0330

    feat: add javascript file

---
+++ src/app.js
@@ -0,0 +1,76 @@
+const flashcards = document.querySelector('.flashcards');
+const createBox = document.querySelector('.create-box');
+const question = document.getElementById('question');
+const answer = document.getElementById('answer');
+const fillInput = document.querySelector('.fill-input');
+
+// let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+// console.log(contentArray)
+let contentArray = [];
+
+const divMaker = (text) => {
+    let div = document.createElement('div');
+    let h2_question = document.createElement('h2');
+    let h2_answer = document.createElement('h2');
+    div.className = 'flashcard';
+    h2_question.className = 'question';
+    h2_answer.className = 'answer';
+    console.log(text)
+    text.forEach(item => {
+        h2_question.innerHTML = item.my_question;
+        h2_answer.innerHTML = item.my_answer;
+        console.log(item.my_question, item.my_answer)
+    })
+    div.appendChild(h2_question);
+    div.appendChild(h2_answer);
+    if (h2_question.textContent === "" || h2_answer.textContent === "") {
+        div.style.display = 'none';
+        showFill();
+    } else {
+        flashcards.appendChild(div);
+    }
+    div.addEventListener('click', () => {
+        if (h2_answer.style.display === 'none') {
+            h2_answer.style.display = 'block'
+        } else {
+            h2_answer.style.display = 'none'
+        }
+    })
+    div.addEventListener('click', () => {
+
+    })
+}
+const addFlashcard = () => {
+    let flashcardInfo = {
+        my_question: question.value,
+        my_answer: answer.value
+    }
+    console.log("answer " + flashcardInfo.my_answer)
+    console.log("question " + flashcardInfo.my_question)
+    contentArray.push(flashcardInfo);
+    // localStorage.setItem('items', JSON.stringify(contentArray));
+    divMaker(contentArray);
+    answer.value = "";
+    question.value = "";
+}
+
+const showCreateCart = () => {
+    createBox.style.display = "block";
+}
+
+const hideFlashCard = () => {
+    createBox.style.display = "none";
+}
+
+const delCards = () => {
+    // localStorage.clear();
+    flashcards.innerHTML = "";
+    contentArray = [];
+}
+
+const showFill = () => {
+    fillInput.style.display = 'block'
+    setTimeout(function () {
+        fillInput.style.display = 'none';
+    }, 3000)
+}
